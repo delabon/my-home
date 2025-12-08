57ca7bcba5d5320b660602a3e65083409160980a
Sabri Taieb <sabritaiebdev@gmail.com>
2025-12-08 09:25:36 +0100

    feat: Show blog posts on homepage

---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { Head } from '@inertiajs/vue3';
+import SimplePagination from "@/components/ui/SimplePagination.vue";
+
+defineProps({
+    posts: {
+        type: Object,
+        required: true,
+    }
+});
+</script>
+
+<template>
+    <Head title="Welcome">
+        <link rel="preconnect" href="https://rsms.me/" />
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+    </Head>
+    <div>
+        <div v-if="posts.data.length > 0">
+            <article v-for="post in posts.data" :key="post.id">
+                <h3>{{ post.title}}</h3>
+            </article>
+        </div>
+        <p v-else>
+            No posts yet!
+        </p>
+
+        <SimplePagination
+            :prevUrl="posts.prev_page_url"
+            :nextUrl="posts.next_page_url"
+        />
+    </div>
+</template>
