65d131f890566c9eb7039c08d1d269e67e6cf21f
Sabri Taieb <sabritaiebdev@gmail.com>
2025-12-12 05:11:21 +0100

    chore: tests for PostStatus enum

---
+++ tests/Unit/Enums/PostStatusTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Enums\PostStatus;
+
+it('returns the correct label', function () {
+    expect(PostStatus::Published->label())->toBe('Published');
+});
+
+it('returns options for the Select component', function () {
+    expect(PostStatus::options())->toBe([
+        [
+            'value' => 'published',
+            'label' => 'Published',
+       ],
+        [
+            'value' => 'draft',
+            'label' => 'Draft',
+      ]
+    ]);
+});
