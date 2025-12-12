231c28e485f86eff6517cfe0808641f68b674449
Sabri Taieb <sabritaiebdev@gmail.com>
2025-12-12 05:12:12 +0100

    chore: add property-read anotations to models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,10 +5,19 @@ declare(strict_types=1);
 namespace App\Models;
 
 use App\Enums\PostStatus;
+use Carbon\CarbonInterface;
 use Illuminate\Contracts\Database\Query\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @property-read int $id
+ * @property-read string $title
+ * @property-read string $body
+ * @property-read PostStatus $status
+ * @property-read CarbonInterface $created_at
+ * @property-read CarbonInterface $updated_at
+ */
 final class Post extends Model
 {
     protected $fillable = [
--- app/Models/User.php
+++ app/Models/User.php
@@ -5,10 +5,21 @@ declare(strict_types=1);
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Carbon\CarbonInterface;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ * @property-read int $id
+ * @property-read string $name
+ * @property-read string $email
+ * @property-read string $password
+ * @property-read CarbonInterface|null $email_verified_at
+ * @property-read string $remember_token
+ * @property-read CarbonInterface $created_at
+ * @property-read CarbonInterface $updated_at
+ */
 final class User extends Authenticatable
 {
     use Notifiable;
