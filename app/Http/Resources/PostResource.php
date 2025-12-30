7cb3a4996ecc8fe568997835841d01c883bb701f
Sabri Taieb <sabritaiebdev@gmail.com>
2025-12-30 10:50:45 +0100

    chore: fix a LaraStan error

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace App\Http\Resources;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @property-read Post $resource
+ */
 final class PostResource extends JsonResource
 {
     /**
