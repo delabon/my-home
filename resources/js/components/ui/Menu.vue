1ff5aa2b02a070a480b17f12088e5da0e6fc843f
Sabri Taieb <sabritaiebdev@gmail.com>
2025-12-31 07:43:56 +0100

    refactor: improve the Menu.vue component

--- resources/js/components/ui/Menu.vue
+++ resources/js/components/ui/Menu.vue
@@ -6,9 +6,11 @@ import {home} from "@/routes";
 </script>
 
 <template>
-    <nav class="flex items-center gap-3">
+    <nav class="flex items-center gap-2">
         <Link :href="home.url()" class="hover:underline hover:text-white">Home</Link>
+        <span class="text-2xl">&bull;</span>
         <A label="LinkedIn" href="https://www.linkedin.com/in/sabri-taieb/" target="_blank"/>
+        <span class="text-2xl">&bull;</span>
         <A label="GitHub" href="https://github.com/delabon/" target="_blank"/>
     </nav>
 </template>
