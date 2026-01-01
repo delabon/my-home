072e0c24aa45202fa20d22cd3bc97916975a037d
Sabri Taieb <sabritaiebdev@gmail.com>
2026-01-01 05:43:02 +0100

    feat: update dashboard page UI

---
+++ resources/js/components/ui/DashboardMenu.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import {Link, usePage} from "@inertiajs/vue3";
+import {dashboard, home} from "@/routes";
+import posts from "@/routes/posts";
+
+const page = usePage();
+</script>
+
+<template>
+    <nav class="flex items-center gap-2">
+        <Link :href="home.url()" class="hover:underline hover:text-white">Home</Link>
+        <span class="text-2xl">&bull;</span>
+        <Link :href="dashboard.url()" class="hover:underline hover:text-white">Dashboard</Link>
+        <span class="text-2xl">&bull;</span>
+        <Link :href="posts.index().url" class="hover:underline hover:text-white">Posts</Link>
+    </nav>
+</template>
--- resources/js/pages/dashboard/Dashboard.vue
+++ resources/js/pages/dashboard/Dashboard.vue
@@ -1,7 +1,19 @@
 <script setup lang="ts">
-import { Head } from '@inertiajs/vue3';
+import {Head, usePage} from '@inertiajs/vue3';
+import DashboardLayout from "@/layouts/DashboardLayout.vue";
+import DashboardMenu from "@/components/ui/DashboardMenu.vue";
+
+const page = usePage();
 </script>
 
 <template>
     <Head title="Dashboard" />
+    <DashboardLayout>
+        <div class="flex flex-col gap-3">
+            <DashboardMenu/>
+            <h1 class="text-3xl text-white">Welcome, {{ page.props.auth.user.name }}!</h1>
+        </div>
+
+        <div class="mt-10"></div>
+    </DashboardLayout>
 </template>
